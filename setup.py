dc436610b0aa6f6dbff54697f59367c87e76b8dd
gwr3n <robros@gmail.com>
2018-04-11 21:08:27 +0100

    Amended setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
   packages = ['inventoryanalytics'], # this must be the same as the name above
   version = '0.2',
   description = 'An Inventory Analytics library.',
-  long_description = 'An Inventory Analytics library (to be expanded).',
   author = 'Roberto Rossi',
   author_email = 'robros@gmail.com',
   url = 'https://github.com/gwr3n/inventoryanalytics', # use the URL to the github repo
