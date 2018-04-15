5006b6e7d39a23405e79e3de213cd436e613da65
gwr3n <robros@gmail.com>
2018-04-15 01:13:11 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
   name = 'inventoryanalytics',
   packages = ['inventoryanalytics'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.4',
   description = 'An Inventory Analytics library.',
   author = 'Roberto Rossi',
   author_email = 'robros@gmail.com',
   url = 'https://github.com/gwr3n/inventoryanalytics', # use the URL to the github repo
-  download_url = 'https://github.com/gwr3n/inventoryanalytics/archive/0.2.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/gwr3n/inventoryanalytics/archive/0.4.tar.gz', # I'll explain this in a second
   keywords = ['inventory', 'analytics'], # arbitrary keywords
   classifiers = [],
 )
