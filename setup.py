0517c287810720fe64f0ac58609f47170ed16095
gwr3n <robros@gmail.com>
2018-04-15 01:43:25 +0100

    Fixed distutil folder tree generation bug

--- setup.py
+++ setup.py
@@ -4,13 +4,26 @@
 from distutils.core import setup
 setup(
   name = 'inventoryanalytics',
-  packages = ['inventoryanalytics'], # this must be the same as the name above
-  version = '0.4',
+  packages = ['inventoryanalytics',
+              'inventoryanalytics.lotsizing',
+              'inventoryanalytics.lotsizing.deterministic',
+              'inventoryanalytics.lotsizing.deterministic.constant',
+              'inventoryanalytics.lotsizing.deterministic.time_varying',
+              'inventoryanalytics.lotsizing.deterministic.time_varying.test',
+              'inventoryanalytics.lotsizing.stochastic',
+              'inventoryanalytics.lotsizing.stochastic.nonstationary',
+              'inventoryanalytics.lotsizing.stochastic.nonstationary.ss_policy',
+              'inventoryanalytics.lotsizing.stochastic.nonstationary.test',
+              'inventoryanalytics.lotsizing.stochastic.stationary',
+              'inventoryanalytics.lotsizing.stochastic.stationary.test',
+              'inventoryanalytics.utils',
+              'inventoryanalytics.utils.test'], # this must be the same as the name above
+  version = '0.5',
   description = 'An Inventory Analytics library.',
   author = 'Roberto Rossi',
   author_email = 'robros@gmail.com',
   url = 'https://github.com/gwr3n/inventoryanalytics', # use the URL to the github repo
-  download_url = 'https://github.com/gwr3n/inventoryanalytics/archive/0.4.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/gwr3n/inventoryanalytics/archive/0.5.tar.gz', # I'll explain this in a second
   keywords = ['inventory', 'analytics'], # arbitrary keywords
   classifiers = [],
 )
