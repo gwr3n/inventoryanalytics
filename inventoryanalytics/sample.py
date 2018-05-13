af60fc31b40e4c596a10d63b9ac84521ce2dfaf3
gwr3n <robros@gmail.com>
2018-05-13 14:30:15 +0100

    Refactored location of sample.py

--- inventoryanalytics/sample.py
+++ inventoryanalytics/sample.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     #pmf = lambda d, k : sp.poisson(d).pmf(k)/q 
     #D = [2,4,6,4]
     #print([[[(i,j), pmf(d, i)*pmf(d, j)] for i in range(0, max_demand(d)) for j in range(0, max_demand(d))] for d in D])
-    a = (k for k in range(1,2))
+    a = list(k for k in range(1,2))
     print(a[0])
