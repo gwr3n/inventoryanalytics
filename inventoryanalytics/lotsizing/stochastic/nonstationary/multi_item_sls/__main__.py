60c2057120b820f5c041f837a35b43d4078b8112
gwr3n <robros@gmail.com>
2018-11-24 16:54:18 +0000

    Amdended jrp instance

--- inventoryanalytics/lotsizing/stochastic/nonstationary/multi_item_sls/__main__.py
+++ inventoryanalytics/lotsizing/stochastic/nonstationary/multi_item_sls/__main__.py
@@ -7,14 +7,14 @@ Optimal order quantity: (4, 4)
 '''
 
 instance = {"K": 10, "v": 0, "h": 1, "p": 5, "d": [3,6,9,6],
-            "max_inv": 15, "q": 0.95, "initial_order": True}
+            "max_inv": 15, "q": 0.99, "initial_order": True}
 lot_sizing = sdp.MultiItemStochasticLotSizing(**instance)
 t = 0       # initial period
 i = (0,0)   # initial inventory level
 print("Optimal policy cost: "    + str(lot_sizing.f(i)))
 print("Optimal order quantity: " + str(lot_sizing.q(t, i)))
 
-#instance["initial_order"]=False
-#lot_sizing = sdp.MultiItemStochasticLotSizing(**instance)
-#points = [(i,j,lot_sizing.f((i,j))) for i in range(0,15) for j in range(0,15)]
-#print(points)
+instance["initial_order"]=False
+lot_sizing = sdp.MultiItemStochasticLotSizing(**instance)
+points = [(i,j,lot_sizing.f((i,j))) for i in range(0,15) for j in range(0,15)]
+print(points)
