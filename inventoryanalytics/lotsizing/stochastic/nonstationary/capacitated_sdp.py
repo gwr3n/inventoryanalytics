8be77453f546f1fa8c3ec801f76268b90e516446
gwr3n <robros@gmail.com>
2020-05-28 19:07:53 +0100

    Refactored capacitated lot sizing

--- inventoryanalytics/lotsizing/stochastic/nonstationary/capacitated_sdp.py
+++ inventoryanalytics/lotsizing/stochastic/nonstationary/capacitated_sdp.py
@@ -13,8 +13,42 @@ import scipy.stats as sp
 import json
 import matplotlib.pyplot as plt
 import numpy.random as rnd
+import signal
 
 import functools 
+import time
+
+class TimeoutException(Exception):
+    pass
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+def deadline(seconds, *args):
+    def decorate(f):
+        def handler(signum, frame):
+            raise TimeoutException("Timed out!")
+
+        def new_f(*args):
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(seconds)
+            try:
+                return f(*args)
+            finally:
+                signal.alarm(0)
+
+        new_f.__name__ = f.__name__
+        return new_f
+    return decorate
 
 class memoize(object): 
     """
@@ -124,7 +158,7 @@ class StochasticLotSizing:
     """
 
     def __init__(self, K: float, B: float, v: float, h: float, p: float, w: float, d: Demand, 
-                 max_inv: float, initial_order: bool):
+                 min_inv: float, max_inv: float, initial_order: bool):
         """
         Create an instance of StochasticLotSizing.
         
@@ -136,13 +170,14 @@ class StochasticLotSizing:
             p {float} -- the proportional unit inventory penalty cost
             w {float} -- the discount factor
             d {Demand} -- the demand
+            min_inv {float} -- the minimum inventory level
             max_inv {float} -- the maximum inventory level
             initial_order {bool} -- allow order in the first period
         """
 
         # initialize instance variables
-        self.T, self.K, self.B, self.v, self.h, self.p, self.w, self.max_inv = len(d.pmf), K, B, v, h, p, w, max_inv
-        self.pmf = d.pmf
+        self.T, self.K, self.B, self.v, self.h = len(d.pmf), K, B, v, h
+        self.p, self.w, self.pmf, self.min_inv, self.max_inv = p, w, d.pmf, min_inv, max_inv
 
         # lambdas
         if initial_order:                                               # action generator
@@ -200,7 +235,7 @@ class StochasticLotSizing:
         #Forward recursion
         v = min(                                                                # optimal cost
             [sum([p[1]*(self.iv(s, a, p[0])+                                    # immediate cost
-                       (self.w*self._f(self.st(s, a, p[0])) if s.t < self.T-1 else 0))   # future cost
+                       (self.w*self._f(self.st(s, a, p[0])) if s.t < self.T-1 else 0)) # future cost
                   for p in self.pmf[s.t]])                                      # demand realisations
              for a in self.ag(s)])                                              # actions
 
@@ -212,7 +247,7 @@ class StochasticLotSizing:
         self.cache_actions[str(s)]=q[0] if bool(q) else None                    # store an action in dictionary
         return v                                                                # return expected total cost
 
-    def extract_sS_policy(self, min_inv) -> List[float]:
+    def extract_sS_policy(self) -> List[float]:
         """
         Extract optimal (sk,Sk) policy parameters
         
@@ -220,12 +255,12 @@ class StochasticLotSizing:
             List[float] -- the optimal sk,Sk policy parameters [...,[s_k,S_k],...]
         """
 
-        for i in range(min_inv, self.max_inv):
+        for i in range(self.min_inv, self.max_inv):
             self.f(i)
         policy_parameters = []
         for t in range(0, len(self.pmf)):
             policy_parameters.append([])
-            level, min_level = self.max_inv - 2, min_inv
+            level, min_level = self.max_inv - 2, self.min_inv
             s, nextState = State(t, level), State(t, level+1)
             while level > min_level:
                 if (self.cache_actions.get(str(s), 0) > 0 and self.cache_actions.get(str(nextState), 0) == 0) or \
@@ -234,10 +269,44 @@ class StochasticLotSizing:
                 level, s, nextState = level - 1, State(t, level - 1), State(t, level)
         return policy_parameters
 
-    def testKBConvexity(self, min_inv) -> bool:
+    @timer
+    def testKBConvexity(self) -> bool:
+        try:
+            return self.testKBConvexity_full()
+        except TimeoutException:
+            print("testKBConvexity_full() has timed out, switching to random checks...")
+            return self.testKBConvexity_random()
+
+    @deadline(60)                                         #KBConvexity check timeout in seconds
+    def testKBConvexity_full(self) -> bool:
+        """
+        Tests CK convexity as originally introduced in Gallego G, Scheller-Wolf A (2000) 
+        Capacitated inventory problems with xed order costs: Some optimal policy structure. 
+        European Journal of Operational Research 126(3):603-613.
+
+        Returns:
+            bool -- true if the function is CK convex
+        """
+        return all([self.K + self.f(x+a) - self.f(x) - a*(self.f(y) - self.f(y-b))/b >= 0
+                    for x in range(self.min_inv+1, self.max_inv-1) 
+                    for y in range(self.min_inv+1, x-1)
+                    for a in range(1, min(self.B+1,self.max_inv-x))
+                    for b in range(1, min(self.B+1,y-self.min_inv))])
+
+    def testKBConvexity_random(self) -> bool:
+        """
+        Tests CK convexity as originally introduced in Gallego G, Scheller-Wolf A (2000) 
+        Capacitated inventory problems with xed order costs: Some optimal policy structure. 
+        European Journal of Operational Research 126(3):603-613.
+
+        Returns:
+            bool -- true if the function is CK convex
+        """
+        N = 1000
+        print("Running "+str(N)+" random checks...")
         step, k = 1, 0
-        while k < 1000:
-            x = rnd.randint(min_inv, self.max_inv-1)
+        while k < N:
+            x = rnd.randint(self.min_inv, self.max_inv-1)
             a = rnd.randint(0,min(self.max_inv-1-x,self.B))      
             gx, gxa, gxd = self.f(x), self.f(x+a), self.f(x+step) - self.f(x)
             if self.K + gxa - gx - a*gxd < 0:
@@ -254,89 +323,108 @@ class StochasticLotSizing:
                 k += 1
         return True
     
-    def testAlwaysOrder(self, domain) -> bool:
+    @timer
+    def testAlwaysOrder(self) -> bool:
+        """Tests if there exists a threshold s such that no order is placed for inventory greater than s
+        and an order is placed for inventory less or equal to s
+
+        Returns:
+            bool -- true if the function satisfies the AlwaysOrder property
+        """
+
         t = 0
-        orders = [lot_sizing_order.q(t, i) for i in range(*domain)]
+        orders = [lot_sizing_order.q(t, i) for i in range(self.min_inv,self.max_inv)]
         while(orders[-1]==0):
             orders.pop(-1)
         return orders.count(0) == 0 
 
 if __name__ == '__main__':
-    domain = (-20,200)          # inventory level domain for plotting
+    plot_domain = (-20,200)          # inventory level domain for plotting and printing
+    plotFunctions = True
+    printFunctions = True
     
     # Shiaoxiang 1996
     # instance = {"K": 22, "B": 9, "v": 0, "h": 1, "p": 10, "w": 0.9, 
     #             "d": PmfDemand([[[6, 0.95], [7, 0.05]] for k in range(20)]), 
-    #             "max_inv": 300, "initial_order": False}
+    #             "min_inv": -10, "max_inv": 300, "initial_order": False}
 
     # Gallego 2000
     # instance = {"K": 10, "B": 10, "v": 0, "h": 1, "p": 9, "w": 0.9, 
     #             "d": PmfDemand([[[1, 0.15], [6, 0.70], [7, 0.15]] for k in range(52)]), 
-    #             "max_inv": 300, "initial_order": False}
+    #             "min_inv": -50, "max_inv": 300, "initial_order": False}
     
     # Rossi et al. 2020
-    instance = {"K": 100, "B": 65, "v": 0, "h": 1, "p": 10, "w": 1, "d": PoissonDemand([20,40,60,40],0.9999), "max_inv": 300, "initial_order": False}
-    # instance = {"K": 100, "B": 65, "v": 0, "h": 1, "p": 10, "w": 1, "d": NormalDemand([20,40,60,40], 0.25 ,0.9999), "max_inv": 300, "initial_order": False}
-    
-    # This cycle also builds the internal map
-    lot_sizing_no_order = StochasticLotSizing(**instance)
-    t = 0                       # initial period
-    for i in range(*domain):    # range over inventory level domain
-        print("Optimal policy cost: "    + str(lot_sizing_no_order.f(i)))
-    
-    # Plot G(y)
-    plt.plot([k for k in range(*domain)], [lot_sizing_no_order.f(k) for k in range(*domain)], label='G(y)')
-    # Print G
-    # print(list(zip([k for k in range(*domain)], [lot_sizing_no_order.f(k) for k in range(*domain)])))
+    instance = {"K": 100, "B": 65, "v": 0, "h": 1, "p": 10, "w": 1, "d": PoissonDemand([20,40,60,40],0.9999), "min_inv": -50, "max_inv": 300, "initial_order": False}
+    # instance = {"K": 100, "B": 65, "v": 0, "h": 1, "p": 10, "w": 1, "d": NormalDemand([20,40,60,40], 0.25 ,0.9999), "min_inv": -50, "max_inv": 300, "initial_order": False}
 
-    # Build action map
+    # Define G(y) and C(y)
+    lot_sizing_no_order = StochasticLotSizing(**instance)   # G(y)
     instance["initial_order"]=True
-    lot_sizing_order = StochasticLotSizing(**instance)
-    for i in range(*domain):    # range over inventory level domain
-        print("Optimal policy cost: "    + str(lot_sizing_order.f(i)))
-        print("Optimal order quantity("+str(i)+"): " + str(lot_sizing_order.q(t, i)))
+    lot_sizing_order = StochasticLotSizing(**instance)      # C(y)
 
-    # Plot C(y)
-    plt.plot([k for k in range(*domain)], [lot_sizing_order.f(k) for k in range(*domain)], label='C(y)')
-    # Print C
-    # print(list(zip([k for k in range(*domain)], [lot_sizing_order.f(k) for k in range(*domain)])))
     
-    # Plot C(y)-G(y)
-    plt.plot([k for k in range(*domain)], [lot_sizing_order.f(k) - lot_sizing_no_order.f(k) for k in range(*domain)], 
-             label='C(y)-G(y)')
-    # print(list(zip([k for k in range(*domain)], [lot_sizing_order.f(k) - lot_sizing_no_order.f(k) for k in range(*domain)])))
+    if plotFunctions: 
+        plt.plot([k for k in range(*plot_domain)],              # Plot G(y)
+                 [lot_sizing_no_order.f(k) 
+                 for k in range(*plot_domain)], label='G(y)')
+    
+        plt.plot([k for k in range(*plot_domain)],              # Plot C(y)
+                 [lot_sizing_order.f(k) 
+                 for k in range(*plot_domain)], label='C(y)')
+    
+        plt.plot([k for k in range(*plot_domain)],              # Plot C(y)-G(y)
+                 [lot_sizing_order.f(k) - lot_sizing_no_order.f(k) 
+                 for k in range(*plot_domain)], label='C(y)-G(y)')
 
-    #Plot Q
-    plt.scatter([k for k in range(*domain)], [lot_sizing_order.q(0,k) for k in range(*domain)], s=2, label='Q')
-    plt.ylabel('Optimal policy cost')
+        plt.scatter([k for k in range(*plot_domain)],           #Plot Q
+                    [lot_sizing_order.q(0,k) for k in range(*plot_domain)], s=2, label='Q')
+    
+    if printFunctions: 
+        print("G(y) = "+str(list(zip([k for k in range(*plot_domain)],        # Print G(y)
+                                     [lot_sizing_no_order.f(k) for k in range(*plot_domain)]))))
+        print()
+        print("C(y) = "+str(list(zip([k for k in range(*plot_domain)],        # Print C(y)
+                                     [lot_sizing_order.f(k) for k in range(*plot_domain)]))))
+        print()
+        print("C(y)-G(y) = "+str(list(zip([k for k in range(*plot_domain)],   # Print C(y)-G(y)
+                                          [lot_sizing_order.f(k) - lot_sizing_no_order.f(k) 
+                                           for k in range(*plot_domain)]))))
+        print()
+        print("Q = "+str(list(zip([k for k in range(*plot_domain)],           # Print Q
+                                  [lot_sizing_order.q(0,k) for k in range(*plot_domain)]))))
 
     # Extract [sk,Sk] Policy 
     print()
     print("****** [sk,Sk] Policy *****")
-    min_inv = -50               # min inventory level
-    skSk_policy = lot_sizing_order.extract_sS_policy(min_inv)   
+    skSk_policy = lot_sizing_order.extract_sS_policy()   
     period = 1
-    for i in skSk_policy:
-        print("Period: "+str(period)+"[sk,Sk]")
-        for j in i:
+    for i in range(len(skSk_policy)):
+        print("Period: "+str(i)+"\n[sk,Sk]")
+        for j in skSk_policy[i]:
             print(j)
         period += 1
         print()
     print("***************************")
 
     print()
-    print("***************************")
+    print("********** Checks *********")
+
+    # Set a suitable timeout in seconds for KBConvexity check
     try:
-        print("The function is (K,B)-convex") if lot_sizing_no_order.testKBConvexity(min_inv) else print("The function is not (K,B)-convex")
+        if lot_sizing_no_order.testKBConvexity():
+            print("The function G(y) is (K,B)-convex")  
+        else: 
+            print("The function G(y) is not (K,B)-convex")
     except Exception as e:
-        print("(K,B)-convexity test failed")
+        print("(K,B)-convexity test for G(y) failed")
         print(str(e))
-    print("***************************")
 
     print()
-    print("***************************")
     try:
-        print("The function satisfies the AlwaysOrder property") if lot_sizing_no_order.testAlwaysOrder(domain) else print("The function does not satisfy the AlwaysOrder property")
+        if lot_sizing_no_order.testAlwaysOrder():
+            print("The function satisfies the AlwaysOrder property")  
+        else: 
+            print("The function does not satisfy the AlwaysOrder property")
     except Exception as e:
         print("AlwaysOrder test failed")
         print(str(e))
