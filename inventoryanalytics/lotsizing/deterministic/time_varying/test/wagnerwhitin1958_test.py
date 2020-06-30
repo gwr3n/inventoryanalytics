ce1816a29b3fa950057377fa0bb5641b8087af56
Roberto Rossi <robros@gmail.com>
2020-06-30 11:02:20 +0100

    Compacted Wagner-Whitin algos in one file

--- inventoryanalytics/lotsizing/deterministic/time_varying/test/wagnerwhitin1958_test.py
+++ inventoryanalytics/lotsizing/deterministic/time_varying/test/wagnerwhitin1958_test.py
@@ -9,30 +9,37 @@ Copyright (c) 2018 Roberto Rossi
 '''
 
 import unittest
-import inventoryanalytics.lotsizing.deterministic.time_varying.wagnerwhitin1958_sp as ww_sp
-import inventoryanalytics.lotsizing.deterministic.time_varying.wagnerwhitin1958_cplex as ww_cplex
+import inventoryanalytics.lotsizing.deterministic.time_varying.wagnerwhitin1958 as ww
 
 class TestWagnerWhitin(unittest.TestCase):
 
     def setUp(self):
         instance = {"K": 30, "h": 1, "d":[10,20,30,40]}
-        self.ww_sp = ww_sp.WagnerWhitin(**instance)
-        self.ww_cplex = ww_cplex.WagnerWhitin(**instance)
+        self.ww_dp = ww.WagnerWhitinDP(**instance)
+        self.ww_cplex = ww.WagnerWhitinCPLEX(**instance)
+
+        instance_I0 = {"K": 30, "h": 1, "d":[10,20,30,40], "I0": 30}
+        self.ww_dp_I0 = ww.WagnerWhitinDPI0(**instance_I0)
+        self.ww_cplex_I0 = ww.WagnerWhitinCPLEX(**instance_I0)
 
     def tearDown(self):
         pass
 
     def test_cycle_cost(self):
-        self.assertEqual(self.ww_sp.cycle_cost(0,2), 110) # 30 + 10*0 + 20*1 + 30*2 = 90
-        self.assertEqual(self.ww_sp.cycle_cost(1,3), 140) # 30 + 20*0 + 30*1 + 40*2 = 120
+        self.assertEqual(self.ww_dp.cycle_cost(0,2), 110) # 30 + 10*0 + 20*1 + 30*2 = 90
+        self.assertEqual(self.ww_dp.cycle_cost(1,3), 140) # 30 + 20*0 + 30*1 + 40*2 = 120
     
     def test_optimal_cost(self):
-        self.assertEqual(self.ww_sp.optimal_cost(), 110) # 30 + 10*0 + 20*1 + 30 + 30*0 + 40*1 = 120
+        self.assertEqual(self.ww_dp.optimal_cost(), 110) # 30 + 10*0 + 20*1 + 30 + 30*0 + 40*1 = 120
         self.assertEqual(self.ww_cplex.optimal_cost(), 110) # 30 + 10*0 + 20*1 + 30 + 30*0 + 40*1 = 120
 
     def test_order_quantities(self):
-        self.assertEqual(self.ww_sp.order_quantities(), [30,0,30,40])
+        self.assertEqual(self.ww_dp.order_quantities(), [30,0,30,40])
         self.assertEqual(self.ww_cplex.order_quantities(), [30,0,30,40])
+
+    def test_order_quantities_I0(self):
+        self.assertEqual(self.ww_dp_I0.order_quantities(), [0,0,30,40])
+        self.assertEqual(self.ww_cplex_I0.order_quantities(), [0,0,30,40])
 
 if __name__ == '__main__':
     unittest.main()
---
+++ inventoryanalytics/lotsizing/deterministic/time_varying/wagnerwhitin1958.py
@@ -0,0 +1,238 @@
+'''
+inventoryanalytics: a Python library for Inventory Analytics
+
+Author: Roberto Rossi
+
+MIT License
+  
+Copyright (c) 2018 Roberto Rossi
+'''
+
+from typing import List
+
+from docplex.mp.model import Model
+import sys
+
+sys.path.insert(0,'/Applications/CPLEX_Studio128/cplex/python/3.6/x86-64_osx')
+
+# http://ibmdecisionoptimization.github.io/docplex-doc/mp/creating_model.html
+# http://www-01.ibm.com/support/docview.wss?uid=swg27042869&aid=1
+
+from typing import List
+import networkx as nx
+import itertools
+
+class WagnerWhitin:
+    """
+    A Wagner-Whitin problem.
+
+    H.M. Wagner and T. Whitin, 
+    "Dynamic version of the economic lot size model," 
+    Management Science, Vol. 5, pp. 89–96, 1958
+    """
+    def __init__(self, K: float, h: float, d: List[float], I0: float):
+        """
+        Create an instance of a Wagner-Whitin problem.
+
+        Arguments:
+            K {float} -- the fixed ordering cost
+            h {float} -- the per unit holding cost
+            d {List[float]} -- the demand in each period
+            I0 {float} -- the initial inventory level
+        """
+
+        self.K, self.h, self.d, self.I0 = K, h, d, I0
+
+class WagnerWhitinCPLEX(WagnerWhitin):
+    """
+    Solves the Wagner-Whitin problem as an MILP.
+    """
+
+    def __init__(self, K: float, h: float, d: List[float], I0: float = 0):
+        """
+        Create an instance of a Wagner-Whitin problem.
+
+        Arguments:
+            K {float} -- the fixed ordering cost
+            h {float} -- the per unit holding cost
+            d {List[float]} -- the demand in each period
+        """
+        super().__init__(K, h, d, I0)
+        self.model()
+
+    def model(self):
+        """
+        Model and solve the Wagner Whitin problem via CPLEX
+        """
+
+        model = Model("Wagner Whitin")
+        T = len(self.d)
+        idx = [t for t in range(0,T)]
+        self.Q = model.continuous_var_dict(idx, name="Q")
+        I = model.continuous_var_dict(idx, name="I")
+        delta = model.binary_var_dict(idx, name="delta")
+
+        for t in range(0,T):
+            model.add_constraint(self.I0 + model.sum(self.Q[k] - self.d[k] for k in range(0,t+1)) == I[t])
+            model.add_constraint(model.if_then(delta[t] == 0, self.Q[t] == 0))
+
+        model.minimize(model.sum(delta[t] * self.K + self.h * I[t] for t in range(0,T)))
+        model.print_information()
+        self.msol = model.solve()
+        if self.msol:
+            model.print_solution()
+            print(model.solution.get_value("Q_2"))
+        else:
+            print("Solve status: " + self.msol.get_solve_status() + "\n")
+        
+    def order_quantities(self) -> List[float]:
+        '''
+        Compute optimal Wagner-Whitin order quantities
+        '''
+        return [self.msol.get_var_value(self.Q[t]) for t in range(0,len(self.d))]
+    
+    def optimal_cost(self) -> float:
+        '''
+        Compute the cost of an optimal solution to the Wagner-Whitin problem
+        '''
+        return self.msol.get_objective_value()    
+
+    @staticmethod
+    def _test():
+        instance = {"K": 30, "h": 1, "d":[10,20,30,40], "I0": 0}
+        ww = WagnerWhitinCPLEX(**instance)
+        ww.model()
+
+class WagnerWhitinDP(WagnerWhitin):
+    """
+    Implements the traditional Wagner-Whitin shortest path algorithm.
+    """
+
+    def __init__(self, K: float, h: float, d: List[float]):
+        """
+        Create an instance of a Wagner-Whitin problem.
+
+        Arguments:
+            K {float} -- the fixed ordering cost
+            h {float} -- the per unit holding cost
+            d {List[float]} -- the demand in each period
+        """
+        super().__init__(K, h, d, 0)
+        self.graph = nx.DiGraph()
+        for i in range(0, len(self.d)):
+            for j in range(i+1, len(self.d)):
+                self.graph.add_edge(i, j, weight=self.cycle_cost(i, j-1))
+
+    def cycle_cost(self, i: int, j: int) -> float:
+        '''
+        Compute the cost of a replenishment cycle covering periods i,...,j
+        '''
+        if i>j: raise Exception('i>j')
+
+        return self.K + self.h * sum([(k-i)*self.d[k] for k in range(i,j+1)]) 
+
+    def optimal_cost(self) -> float:
+        '''
+        Compute the cost of an optimal solution to the Wagner-Whitin problem
+        '''
+        T, cost, g = len(self.d), 0, self.graph
+        path = nx.dijkstra_path(g, 0, T-1)
+        path.append(len(self.d))
+        for t in range(1,len(path)):
+            cost += self.cycle_cost(path[t-1],path[t]-1)
+            print("c("+str(path[t-1])+","+str(path[t]-1)+") = "+str(self.cycle_cost(path[t-1],path[t]-1)))
+        return cost
+
+    def order_quantities(self) -> List[float]:
+        '''
+        Compute optimal Wagner-Whitin order quantities
+        '''
+        T, g = len(self.d), self.graph
+        path = nx.dijkstra_path(g, 0, T-1)
+        path.append(len(self.d))
+        qty = [0 for k in range(0,T)]
+        for t in range(1,len(path)):
+            qty[path[t-1]] = sum([self.d[k] for k in range(path[t-1],path[t])])
+        return qty
+
+    @staticmethod
+    def _test():
+        instance = {"K": 30, "h": 1, "d":[10,20,30,40]}
+        ww = WagnerWhitinDP(**instance)
+        print("Cost of an optimal plan: ", ww.optimal_cost())
+        print("Optimal order quantities: ", ww.order_quantities())
+
+class WagnerWhitinDPI0(WagnerWhitin):
+    """
+    Extension of the original Wagner-Whitin algorithm 
+    to embed a nonnegative initial inventory.
+    """
+
+    def __init__(self, K: float, h: float, d: List[float], I0: float):
+        """
+        Create an instance of a Wagner-Whitin problem.
+
+        Arguments:
+            K {float} -- the fixed ordering cost
+            h {float} -- the per unit holding cost
+            d {List[float]} -- the demand in each period
+        """
+        super().__init__(K, h, d, I0)
+        self.graph = nx.DiGraph()
+        for i in range(0, len(self.d)):
+            for j in range(i+1, len(self.d)):
+                self.graph.add_edge(i, j, weight=self.cycle_cost(i, j-1))
+
+    def cycle_cost(self, i: int, j: int) -> float:
+        '''
+        Compute the cost of a replenishment cycle covering periods i,...,j
+        '''
+        if i>j: raise Exception('i>j')
+
+        cost_with_order = self.K + \
+                          self.h * sum([(k-i)*self.d[k] for k in range(i,j+1)]) 
+                          
+        if i == 0 and sum(self.d[0:j+1]) <= self.I0:
+            cost_no_order = self.h * sum([(k-i)*self.d[k] for k in range(i,j+1)]) + \
+                            self.h * (self.I0-sum(self.d[0:j+1]))
+            return min(cost_with_order, cost_no_order)
+        elif i >= 0 and sum(self.d[0:j+1]) <= self.I0:
+            return sys.maxsize
+
+        return cost_with_order
+
+    def optimal_cost(self) -> float:
+        '''
+        Compute the cost of an optimal solution to the Wagner-Whitin problem
+        '''
+        T, cost, g = len(self.d), 0, self.graph
+        path = nx.dijkstra_path(g, 0, T-1)
+        path.append(len(self.d))
+        for t in range(1,len(path)):
+            cost += self.cycle_cost(path[t-1],path[t]-1)
+            print("c("+str(path[t-1])+","+str(path[t]-1)+") = "+str(self.cycle_cost(path[t-1],path[t]-1)))
+        return cost
+
+    def order_quantities(self) -> List[float]:
+        '''
+        Compute optimal Wagner-Whitin order quantities
+        '''
+        T, g = len(self.d), self.graph
+        path = nx.dijkstra_path(g, 0, T-1)
+        path.append(len(self.d))
+        qty = [0 for k in range(0,T)]
+        for t in range(1,len(path)):
+            qty[path[t-1]] = sum([self.d[k] for k in range(path[t-1],path[t])]) if sum(self.d[0:path[t-1]+1]) > self.I0 else 0
+        return qty
+
+    @staticmethod
+    def _test():
+        instance = {"K": 30, "h": 1, "d":[10,20,30,40], "I0": 40}
+        ww = WagnerWhitinDPI0(**instance)
+        print("Cost of an optimal plan: ", ww.optimal_cost())
+        print("Optimal order quantities: ", ww.order_quantities())
+
+if __name__ == '__main__':
+    # WagnerWhitinDP._test()
+    WagnerWhitinDPI0._test()
+    # WagnerWhitinCPLEX._test()
