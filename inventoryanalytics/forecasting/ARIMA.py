b91cd14f315558160a8f37b1fcc23ee45f27f003
Roberto Rossi <robros@gmail.com>
2020-09-29 12:10:21 +0100

    Amended ARIMA estimation

--- inventoryanalytics/forecasting/ARIMA.py
+++ inventoryanalytics/forecasting/ARIMA.py
@@ -1,4 +1,4 @@
-import statsmodels.api as sm, pandas as pd
+import statsmodels.api as sm, pandas as pd, statistics
 from statsmodels.tsa.arima_model import ARIMA
 import matplotlib.pyplot as plt
 
@@ -7,6 +7,8 @@ airpass = sm.datasets.get_rdataset("AirPassengers", "datasets")
 ts = pd.Series(airpass.data["value"])
 ts = ts.astype(float)
 model = ARIMA(ts[0:t], order=(0,1,0))
-fitted = model.fit()
-fitted.plot_predict(start=ts.index[3], end=ts.index[-1], alpha=0.1)
+res = model.fit()
+print(res.summary())
+res.plot_predict(start=ts.index[3], end=ts.index[-1], alpha=0.1)
+print("Std residuals: "+str(statistics.stdev(res.resid)))
 plt.show()
