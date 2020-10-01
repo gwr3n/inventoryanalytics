302ef08af1296ea1266a856ad9445649a7c8b9cd
Roberto Rossi <robros@gmail.com>
2020-10-01 21:13:05 +0100

    Added plot of C(Q)

--- inventoryanalytics/lotsizing/stochastic/newsvendor.py
+++ inventoryanalytics/lotsizing/stochastic/newsvendor.py
@@ -1,5 +1,5 @@
+import numpy as np, scipy.integrate as integrate, matplotlib.pyplot as plt
 from scipy.stats import norm
-import scipy.integrate as integrate
 from scipy.optimize import minimize
 
 class Newsvendor:
@@ -11,6 +11,9 @@ class Newsvendor:
 
     def cfolf(self,Q): # complementary first order loss function
         return integrate.quad(lambda x: norm.cdf(x, loc=self.mean, scale=self.std), 0, Q)[0]
+    
+    def folf(self,Q): # first order loss function
+        return self.cfolf(Q)-self.u*(Q - self.mean)
 
     def C(self,Q): # C(Q)
         return (self.o+self.u)*self.cfolf(Q)-self.u*(Q - self.mean)
@@ -23,3 +26,9 @@ nb = Newsvendor(instance)
 print("Q^*=" + str(nb.crit_frac_solution()))
 print("C(Q^*)=" + str(nb.C(nb.crit_frac_solution())))
 print(nb.optC())
+
+plt.plot([nb.C(Q) for Q in np.arange(7,20,0.1)], label="C(Q)")
+plt.plot([nb.folf(Q) for Q in np.arange(7,11,0.1)], label="uE[max(d-Q,0)]")
+plt.plot([nb.cfolf(Q) for Q in np.arange(7,20,0.1)], label="oE[max(Q-d,0)]")
+plt.legend()
+plt.show()
