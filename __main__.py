f9c50d6e70eb6ea15b9822dbe8d0d1b6b15c3bb6
Roberto Rossi <robros@gmail.com>
2024-05-01 17:40:07 +0100

    changed main

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
-import inventoryanalytics.lotsizing.stochastic.stationary.zhengfedergruen1991 as zf
-zf.ZhengFedergruen.run_instance()
+#import inventoryanalytics.lotsizing.stochastic.stationary.zhengfedergruen1991 as zf
+#zf.ZhengFedergruen.run_instance()
 
-# import inventoryanalytics.lotsizing.stochastic.nonstationary.sdp as sdp
-# sdp.StochasticLotSizing.run_instance()
+import inventoryanalytics.lotsizing.stochastic.nonstationary.sdp as sdp
+sdp.StochasticLotSizing.run_instance()
