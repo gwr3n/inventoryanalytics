2fbea36a2ede19b88ae66a613b7e41098f582606
Roberto Rossi <robros@gmail.com>
2024-11-07 04:58:18 +0000

    Implemented fast DP

--- inventoryanalytics/lotsizing/stochastic/nonstationary/RS_shortest_path.py
+++ inventoryanalytics/lotsizing/stochastic/nonstationary/RS_shortest_path.py
@@ -12,11 +12,51 @@ import sys
 from typing import List
 import time
 import math
+import functools 
 
 import networkx as nx
 import scipy.stats as stats
 
+class memoize(object): 
+    """
+    Memoization utility
+    """
+
+    def __init__(self, func): 
+        self.func = func 
+        self.memoized = {} 
+        self.method_cache = {} 
+
+    def __call__(self, *args): 
+        return self.cache_get(self.memoized, args, 
+            lambda: self.func(*args)) 
+
+    def __get__(self, obj, objtype): 
+        return self.cache_get(self.method_cache, obj, 
+            lambda: self.__class__(functools.partial(self.func, obj))) 
+
+    def cache_get(self, cache, key, func): 
+        try: 
+            return cache[key] 
+        except KeyError: 
+            cache[key] = func() 
+            return cache[key] 
+    
+    def reset(self):
+        self.memoized = {} 
+        self.method_cache = {} 
+
+def minimize(function, xmin, xmax):
+    while xmin < xmax:
+        xmid = (xmin + xmax) // 2
+        if function(xmid) < function(xmid + 1):
+            xmax = xmid
+        else:
+            xmin = xmid + 1
+    return {"min_value": function(xmin), "argmin": xmin}
+
 @staticmethod
+@memoize
 def cfolf(lmbda, Q):
     """
     Compute the complementary first order loss function for a Poisson random variable with rate lambda and initial inventory Q.
@@ -28,13 +68,11 @@ def cfolf(lmbda, Q):
     Returns:
     float: The value of the complementary first order loss function.
     """
-    loss = 0
-    for k in range(int(Q)):
-        #loss += (Q - k) * (math.exp(-lmbda) * (lmbda ** k) / math.factorial(k))
-        loss += stats.poisson.cdf(k, lmbda)
+    loss = sum(stats.poisson.cdf(k, lmbda) for k in range(int(Q)))
     return loss
 
 @staticmethod
+@memoize
 def folf(lmbda, Q):
     """
     Compute the first order loss function for a Poisson random variable with rate lambda and initial inventory Q.
@@ -96,29 +134,33 @@ class RS_DP(StochasticLotSizingProblem):
         """
         super().__init__(K, h, p, d, I0)
 
-        if (not math.isnan(I0)):
-            self.cycle_cost = self.cycle_cost_I0
-
         self.graph = nx.DiGraph()
+        start_time = time.time()
         for i in range(0, len(self.d)):
-            for j in range(i+1, len(self.d)):
+            for j in range(i+1, len(self.d) + 1):
                 self.graph.add_edge(i, j, weight=self.cycle_cost(i, j-1))
-        
-        # Print the connection matrix
-        adj_matrix = nx.adjacency_matrix(self.graph).todense()
-        print("Connection Matrix:")
-        print(adj_matrix)
+        self.precompute_time = time.time() - start_time
 
+        # Print the connection matrix
+        # adj_matrix = nx.adjacency_matrix(self.graph).todense()
+        # print("Connection Matrix ("+str(round(self.precompute_time, 2))+"):")
+        # print(adj_matrix) 
+
+    @memoize
     def cycle_cost(self, i: int, j: int) -> float:
         '''
         Compute the expected total cost of a replenishment cycle covering periods i,...,j
         '''
         if i>j: raise Exception('i>j')
 
-        expTotalCost = lambda Q : self.K + sum([self.h*cfolf(sum([self.d[j] for j in range(i,k+1)]),Q) + self.p*folf(sum([self.d[j] for j in range(i,k+1)]),Q) for k in range(i,j+1)])
+        expTotalCost = lambda Q : sum([self.h*cfolf(sum([self.d[j] for j in range(i,k+1)]),Q) + self.p*folf(sum([self.d[j] for j in range(i,k+1)]),Q) for k in range(i,j+1)])
 
-        return min([expTotalCost(k) for k in range(int(sum(self.d)+3*math.sqrt(sum(self.d))))])
+        if i == 0 and not math.isnan(self.I0): 
+            return expTotalCost(self.I0) # cost no order
+        else: 
+            return self.K + minimize(expTotalCost, 0, int(sum(self.d)+3*math.sqrt(sum(self.d))))["min_value"]
     
+    @memoize
     def cycle_S(self, i: int, j: int) -> float:
         '''
         Compute the order up to level of a replenishment cycle covering periods i,...,j
@@ -127,22 +169,10 @@ class RS_DP(StochasticLotSizingProblem):
 
         expTotalCost = lambda Q : self.K + sum([self.h*cfolf(sum([self.d[j] for j in range(i,k+1)]),Q) + self.p*folf(sum([self.d[j] for j in range(i,k+1)]),Q) for k in range(i,j+1)])
 
-        values = [expTotalCost(k) for k in range(int(sum(self.d)+3*math.sqrt(sum(self.d))))]
-        return values.index(min(values))
-
-    def cycle_cost_I0(self, i: int, j: int) -> float:
-        '''
-        Compute the cost of a replenishment cycle covering periods i,...,j
-        when initial inventory is nonzero
-        '''
-        if i>j: raise Exception('i>j')
-
-        expTotalCost = lambda Q : sum([self.h*cfolf(sum([self.d[j] for j in range(i,k+1)]),Q) + self.p*folf(sum([self.d[j] for j in range(i,k+1)]),Q) for k in range(i,j+1)])
-
-        if i == 0: 
-             return expTotalCost(self.I0) # cost no order
-        else: 
-            return self.K + min([expTotalCost(k) for k in range(int(sum(self.d)+3*math.sqrt(sum(self.d))))]) # cost with order
+        if i == 0 and not math.isnan(self.I0): 
+            return self.I0
+        else:
+            return minimize(expTotalCost, 0, int(sum(self.d)+3*math.sqrt(sum(self.d))))["argmin"]
 
     def optimal_cost(self) -> float:
         '''
@@ -153,12 +183,12 @@ class RS_DP(StochasticLotSizingProblem):
         path.append(len(self.d))
         for t in range(1,len(path)):
             cost += self.cycle_cost(path[t-1],path[t]-1)
-            #print("c("+str(path[t-1])+","+str(path[t]-1)+") = "+str(self.cycle_cost(path[t-1],path[t]-1)))
+            # print("c("+str(path[t-1])+","+str(path[t]-1)+") = "+str(self.cycle_cost(path[t-1],path[t]-1)))
         return cost
     
     def order_up_to_level(self) -> List[float]:
         '''
-        Compute optimal Wagner-Whitin order quantities
+        Compute optimal order up to levels
         '''
         T, g = len(self.d), self.graph
         path = nx.dijkstra_path(g, 0, T-1)
@@ -167,15 +197,65 @@ class RS_DP(StochasticLotSizingProblem):
         for t in range(1,len(path)):
             qty[path[t-1]] = self.cycle_S(path[t-1],path[t]-1)
         return qty
+    
+    @staticmethod
+    def reorder_points(instance) -> List[float]:
+        '''
+        Compute the reorder points
+        '''
+        ins = {"K": instance["K"], "h": instance["h"], "p": instance["p"], "d":instance["d"].copy()}
+        d = instance["d"].copy()
+        s = []
+        S = []
+        while len(d) > 0:
+            ww = RS_DP(**ins)
+            optCost = ww.optimal_cost()
+            S0 = ww.order_up_to_level()[0]
+            S.append(S0)
+            cost = optCost
+            I0 = S0
+            while cost - optCost < ins["K"]:
+                I0 -= 1
+                instance_I0 = {"K": ins["K"], "h": ins["h"], "p": ins["p"], "d":d, "I0":I0}
+                ww = RS_DP(**instance_I0)
+                cost = ww.optimal_cost()
+                # print('.', end='', flush=True)
+            s.append(I0)
+            d.pop(0)
+            ins["d"] = d
+        return {"S": S, "s": s}
+    
+    def simulate_sS(self, s, S, initial_inventory):
+        '''
+        Simulate an (s,S) policy
+        '''
+        ETC = 0
+        replications = 10000
+        realisations = [stats.poisson.rvs(self.d[t], size=replications) for t in range(len(self.d))]
+        for r in range(replications):
+            inv = initial_inventory
+            cost = 0
+            for t in range(len(self.d)):
+                if inv <= s[t]:
+                    a = S[t] - inv
+                    inv = S[t] - realisations[t][r]
+                    cost += self.K 
+                else:
+                    inv -= realisations[t][r]
+                cost += self.h*max(inv,0)+self.p*max(-inv,0)
+            ETC += cost
+        return ETC/replications
 
     @staticmethod
     def _test():
-        instance = {"K": 50, "h": 1, "p": 10, "d":[30,40,30,40,30]}
+        instance = {"K": 50, "h": 1, "p": 10, "d":[30,40,30,40,30,30,40,30,40,30]}
         ww = RS_DP(**instance)
         start_time = time.time()
         optCost = ww.optimal_cost()
         end_time = time.time() - start_time
-        print({'optCost': optCost, 'solTime': round(end_time, 2),'S': ww.order_up_to_level()})
+        res = ww.reorder_points(instance)
+        print({'optCost': optCost, 'solTime': round(end_time + ww.precompute_time, 2),'S': res["S"], 's': res["s"]})
+        print("Simulated cost: " + str(round(ww.simulate_sS(res["s"],res["S"],0),2)))
 
 if __name__ == '__main__':
     RS_DP._test()
